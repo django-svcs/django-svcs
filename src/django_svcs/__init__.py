8cbc81f01c32fab055254de343a34c72798ec470
Chris Rose <offline@offby1.net>
2024-08-25 16:50:08 -0700

    More testing

--- src/django_svcs/__init__.py
+++ src/django_svcs/__init__.py
@@ -3,7 +3,7 @@ from .apps import close_registry, get_registry, svcs_from
 __all__ = ["close_registry", "get_registry", "svcs_from"]
 
 try:
-    from ._version import __version__, __version_tuple__
+    from ._version import __version__, __version_tuple__  # type: ignore
 except ImportError:
     __version__ = "0.0.0"
     __version_tuple__ = (0, 0, 0)
